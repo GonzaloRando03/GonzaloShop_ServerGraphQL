629487ed48f259a278770bf0028f47668b8654a0
Gonzalo Rando Serna <gonzalorando03@gmail.com>
2023-02-10 19:57:16 +0100

    expressGraphQL added

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer } = require('apollo-server-express')
 const typeDefs = require('./graphql/types')
 const resolvers = require('./graphql/resolvers')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json({limit: "50mb", extended: true}))
+app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit: 100000}))
+
+app.use(cors())
+
+app.use(bodyParser.json())
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  introspection: true,
+  typeDefs,
+  resolvers,
+  formatError: error => {
+    return error
+  },
+  context: ({ req, res }) => {
+    return {
+      req,
+      res,
+    }
+  },
 })
-  
-server.listen().then(({ url, subscriptionsUrl }) => {
-    console.log(`Server ready at ${url}`)
-    console.log(`Subscriptions ready at ${subscriptionsUrl}`)
+
+server.start().then(()=>{
+    server.applyMiddleware({ app, path: "/graphql" })
+    app.listen(4000, () => {
+        console.log("app is listening to port 4000")
+    })
 })
+
+
+
+
+
+
